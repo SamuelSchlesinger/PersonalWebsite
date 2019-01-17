e7ea82ea2cef05a47e4fb62f731ea41841915172
SamuelSchlesinger <sgschlesinger@gmail.com>
2019-01-17 19:29:05 +0000

    Made the sketch

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,19 +1,93 @@
-var canvas;
+var canvas, world;
 
-var t = 0;
+var n = 100;
+var G = 0.01;
+
+function Particle(p, v, m) {
+  this.id = Math.random() + Math.random() + Math.random();
+  this.p = p;
+  this.v = v;
+  this.a = createVector(0, 0);
+  this.m = m;
+}
+  
+Particle.prototype.update = function() {
+  this.p.add(this.v);
+  this.v.add(this.a);
+  this.a = createVector(0, 0);
+}
+
+Particle.prototype.forceBy = function(particle) {
+  b = createVector(0, 0);
+  b.sub(this.p);
+  b.add(particle.p);
+  r = b.mag();
+  b.heading();
+  b.mult(G * (particle.m * this.m)/(r*r));
+  return b;
+}
+
+function newWorld(n) {
+
+}
+
+function updateWorld(world) {
+  var i = 0;
+  while (i < world.length) { 
+    var j = 0;
+    while (j < world.length) {
+      if (world[i].id != world[j].id) {
+        force = world[i].forceBy(world[j]);
+        force.div(world[i].m);
+        world[i].a.add(force);
+      }
+      j++;
+    }
+    i++;
+  }
+  i = 0;
+  while (i < world.length) {
+    world[i++].update();
+  }
+}
+
+function displayWorld(world) {
+  var i = 0;
+  while (i < world.length) {
+    let radius = world[i].m * 10 + 10;
+    noStroke();
+    if (world[i].id < 1) {
+      fill(world[i].id/3 * 255, 0, 0);
+    } else if (world[i].id < 2) {
+      fill(0, world[i].id/3 * 255, 0);
+    } else {
+      fill(0, 0, world[i].id/3 * 255);
+    }
+    ellipse(world[i].p.x, world[i].p.y, radius, radius);  
+    i++;
+  }
+}
 
 function setup() {
-  canvas = createCanvas(windowWidth / 2, windowHeight / 2);
+  canvas = createCanvas(windowWidth/1.2, windowHeight/1.4);
   canvas.parent('sketch');
+  var i = 0;
+  world = [];
+  while (i < n) {
+    let p = createVector(Math.random() * width, Math.random() * height);
+    let v = createVector(0.01 * (Math.random() - 0.5), 0.01 * (Math.random() - 0.5));
+    let a = new Particle(p, v, Math.random()*10 + 0.05);
+    world.push(a);
+    i++;
+  }
 }
 
 function draw() {
-  background(Math.abs(Math.sin(t)) * 255);
-  fill(Math.abs(Math.cos(t)) * 255);
-  ellipse(Math.sin(t)*(windowWidth / 4) + 200, Math.sin(t*10) * 5 + (windowHeight / 4), 10, 10)
-  t += 0.01;
+  background(0);
+  updateWorld(world);
+  displayWorld(world);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth / 2, windowHeight / 2);
+  resizeCanvas(windowWidth/1.2, windowHeight/1.4);
 }
--- server.js
+++ server.js
@@ -4,29 +4,43 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const readline = require('readline');
 
+// Firstly, we construct a dictionary of American English from the words file
 const rl = readline.createInterface({
   input: require("fs").createReadStream("/usr/share/dict/words")
 });
 
+// dictionary[word] = false means that you are currently inactive
+// dictionary[word] = undefined means that you are not a word
+// These both evaluate to false in conditionals and sometimes these 
+// cases need not be discriminated.
 var dictionary = {}
 
+// Every time we read a new word, we add it in lowercase to the dictionary
 rl.on('line', (input) => {
-  dictionary[input] = true;
+  dictionary[input.toLowerCase()] = true;
 });
 
-    
-rl.on('close', () => {
-  alarm = true;
+// Set an alarm for when we're done setting up the dictionary
+var alarm = false;
+
+rl.on('pause', () => {
+  console.log("Dictionary constructed");
 });
 
+// Using EJS as the templating engine so that we can stick to more JavaScript
 app.set('view engine', 'ejs');
+
+// Serving css and js from the public directory
 app.use(express.static(__dirname + '/public'));
+
+// Using bodyparser to send forms
 app.use(bodyParser.urlencoded({
   enabled: true,
   extended: true
 }));
 app.use(bodyParser.json());
 
+// The following GET requests should be self explanatory
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -39,10 +53,18 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+app.get('/sketch', (req, res) => {
+  res.render('sketch');
+});
+
+// Conceptually, we imagine that there is a list of words we are not allowed to use
+// at the moment, due to the fact that someone already used it. This function removes
+// the given word from this list.
 function remove(word) {
   dictionary[word] = true;
 }
 
+// Generate all of the words which are the given word without a single letter.
 function oneLetterRemoved(word) {
   const length = word.length;
   let i = 1;
@@ -54,6 +76,7 @@ function oneLetterRemoved(word) {
   return words;
 }
 
+// Check if any of these words are in the dictionary.
 function wordsInDictionary(words) {
   var i = 0;
   while (i < words.length) {
@@ -66,14 +89,16 @@ function wordsInDictionary(words) {
   return false;
 }
 
+// Here, we deal with the logic for the CAPTCHA on the contact page.
 app.post('/contact', (req, res) => {
   var word = req.body.word.toLowerCase();
   var words = oneLetterRemoved(word);
   if (word.length < Math.floor(Math.random() * 3) + 3) {
     res.render('failure', {reason: "Pathetic, robot, you can't even think of a long word?"});
-  }
-  else if (!dictionary[word]) { // check if the word has been used recently
+  } else if (!(word in dictionary)) { // check if the word has been used recently
     res.render('failure', {reason: "Filthy robot, that's not even a word..."});
+  } else if (!dictionary[word]) {
+    res.render('failure', {reason: "That word is out of commission because someone has used it in the last 60 seconds"});
   } else if (wordsInDictionary(words)) {
     dictionary[word] = false;
     setTimeout(remove, 60000, word);
@@ -81,10 +106,6 @@ app.post('/contact', (req, res) => {
   } else {  
     res.render('failure', {reason: "Where's the word in there, metalskin?"});
   }
-});
-
-app.get('/sketch', (req, res) => {
-  res.render('sketch');
 });
 
 app.listen(80, () => {
